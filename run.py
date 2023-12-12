5d4dab212777d4a7a1557387ba959963ae168629
ArchinDeepakTV <archindeepakad.ad@gmail.com>
2023-12-12 17:44:35 +0530

    added session object

--- run.py
+++ run.py
@@ -1,5 +1,4 @@
-from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, g
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -12,41 +11,61 @@ app.config.update(
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 )
 
+
 db = SQLAlchemy(app)
+
+
+#! Before Request
+@app.before_request
+def someFunction():
+    g.string = '<br>This Code ran before any request'
 
 
 # ! BASIC ROUTE
 @app.route('/')
 @app.route('/login')
 def loginPage():
-    return render_template('loginPage.html')
+    return render_template('loginPage.html', g.string)
 
 
-# @app.route('/conveyanceForm')
+# ! Route for Distance Based Conveyance Form
 @app.route('/conveyanceForm/<name>/<empCode>')
 @app.route('/conveyanceForm/<name>/<empCode>/')
 def distanceBasedConveyanceEntry(name='Archin Deepak', empCode='THLL0001'):
     return render_template('distanceFormPage.html', name=name, empCode=empCode)
 
 
+# ! Route for non-distance Based Conveyance Form
 @app.route('/conveyanceForm/<name>/<empCode>/a')
 @app.route('/conveyanceForm/<name>/<empCode>/a/')
 def conveyanceEntry(name, empCode):
     return render_template('formPage.html', name=name, empCode=empCode)
 
 
+# ! Route to select which records to view
 @app.route('/records/<name>/<empCode>')
 @app.route('/records/<name>/<empCode>/')
 def recordView(name, empCode):
     return render_template('recordPage.html', name=name, empCode=empCode)
 
 
+# ! Route to view records of selected month and year
 @app.route('/records/<name>/<empCode>/<month>/<year>')
 @app.route('/records/<name>/<empCode>/<month>/<year>/')
 def individualRecordView(name, empCode, month, year):
     return render_template('recordViewPage.html', name=name, empCode=empCode, month=month, year=year)
 
 
+#! session object
+@app.route('/session')
+@app.route('/session/')
+def sessionData():
+    if 'name' not in session:
+        session['name'] = 'harry'
+    return render_template('session.html', session=session, name=session['name'])
+
+
+# ! For creating and working on a database named users
 class User(db.Model):
     __tablename__ = 'users'
 
@@ -64,6 +83,7 @@ class User(db.Model):
         return 'The ID is {}, Name is {}'.format(self.ID, self.name)
 
 
+# ! For creating and working on a database named conveyance
 class Conveyance(db.Model):
     __tablename__ = 'conveyance'
 
@@ -91,6 +111,7 @@ class Conveyance(db.Model):
         return '{} to {} : Travel of {}km'.format(self.From, self.To, self.Distance)
 
 
+# ! main function
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
